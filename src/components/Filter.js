7276a7d8af6374a3391bb329e06a7516991bcd80
WinnieAtieno <winnieotieno773@gmail.com>
2024-08-08 14:22:15 +0300

    completed lab

---
+++ src/components/Filter.js
@@ -0,0 +1,11 @@
+function Filter ({selectedCategory,onCategoryChange}){
+    return (
+    <select name="filter" onChange={onCategoryChange} value={selectedCategory}>
+    <option value="All">Filter by category</option>
+    <option value="Produce">Produce</option>
+    <option value="Dairy">Dairy</option>
+    <option value="Dessert">Dessert</option>
+  </select>
+    )
+}
+export default Filter
